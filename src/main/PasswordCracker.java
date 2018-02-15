b8e57801718b326f879af4a936d11fb84ec0763f
rajathalex <rajathalex@gmail.com>
2018-02-14 21:17:07 -0500

    Testing

--- src/main/PasswordCracker.java
+++ src/main/PasswordCracker.java
@@ -9,52 +9,98 @@ import java.util.*;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
-import java.util.Base64.Decoder;
 
 
 public class PasswordCracker {
 
 	public static void main(String[] args) {
 		try {
+				
+				List<byte[]> decodedSalt = new ArrayList<byte[]>();
+				Map<byte[], byte[]> decodedSaltToDecodedPassword = new HashMap<byte[], byte[]>();
+				Map<byte[], String> decodedSaltToUsername = new HashMap<byte[], String>();
+				Map<String, String> usernameToPlaintextPassword = new HashMap<String, String>();
+				
+				//Input Password File
 				File passwordsFile = new File("resources/pswd.txt");
 				FileInputStream passwordStream = new FileInputStream(passwordsFile);
 				BufferedReader passwordBuffer = new BufferedReader(new InputStreamReader(passwordStream));
-				
-				Map<String, String> usernameToDecodedPassword = new HashMap<String, String>();
-				Map<String, String> usernameToDecodedSalt = new HashMap<String, String>();
-				
-				
+		
 				//Parse the buffer to extract usernames and passwords
-				String password_file_line = null;
-				while ((password_file_line = passwordBuffer.readLine()) != null) {
-					String[] splitLine = password_file_line.split("(\\s)*:(\\s)*");
+				String passwordFileLine = null;
+				while ((passwordFileLine = passwordBuffer.readLine()) != null) {
+					String[] splitLine = passwordFileLine.split("(\\s)*:(\\s)*");
 					
 					if(!splitLine[0].equals("username")) { //Not storing first line which is header
 						//System.out.println(splitLine[0]+" - "+splitLine[1]+" - "+splitLine[2]+" - "+splitLine[3]);
 						
-						
-						usernameToDecodedPassword.put(splitLine[0], splitLine[3]);
-						usernameToDecodedSalt.put(splitLine[0], splitLine[1]);
+						decodedSalt.add(Base64.getDecoder().decode(splitLine[1]));//Decoding Base64 Salt and adding to List
+						decodedSaltToDecodedPassword.put(Base64.getDecoder().decode(splitLine[1]), Base64.getDecoder().decode(splitLine[3]));
+						//System.out.println(Base64.getDecoder().decode(splitLine[3]));
+						decodedSaltToUsername.put(Base64.getDecoder().decode(splitLine[1]), splitLine[0]);
 					}
-					
-					
-					
-				/*
-					//First case: password hashed with no salt
-					if(splited.length == 3){
-						non_salted_passwords.put(splited[0], splited[2]);
-					}
-					
-					//Second case: password hashed with a salt
-					else{
-						salted_passwords.put(splited[0], splited[3]);
-						salted_passwords_salts.put(splited[0], splited[2]);
-					}
-					*/
 				}
 			
 				//Closing Buffer - Done reading password file
 				passwordBuffer.close();
+				
+//				for(byte[] salt: decodedSalt) {
+//					System.out.println(salt.toString());
+//				}
+				
+				String test = Base64.getEncoder().encodeToString(decodedSaltToDecodedPassword.get(Base64.getDecoder().decode("B9OGLTbJNATU+ZJdnaUUGnMe4hOeK9qRW/6zG+Lkn0E=")));
+				System.out.println(test);
+				
+				//Input Dictionary1
+				File dictionaryFile1 = new File("resources/test.txt");
+				FileInputStream dictionaryStream1 = new FileInputStream(dictionaryFile1);
+				BufferedReader dictionaryBuffer1 = new BufferedReader(new InputStreamReader(dictionaryStream1));
+				
+				String dictionaryFileLine = null;
+				while ((dictionaryFileLine = dictionaryBuffer1.readLine()) != null) {
+					char [] plaintextPassword = dictionaryFileLine.trim().toCharArray();
+					//System.out.println(plaintextPassword);
+										
+					for(byte[] salt: decodedSalt) {//Checking all available salts
+						byte[] computedHashedPwd = hashPassword(plaintextPassword,salt,1,256);//Computing Hashed Password from Dictionary					
+						//System.out.println(computedHashedPwd);
+						if(Arrays.equals(computedHashedPwd, decodedSaltToDecodedPassword.get(salt)))
+							usernameToPlaintextPassword.put(decodedSaltToUsername.get(salt), dictionaryFileLine.trim());//Making an entry if match found
+								
+					}	
+					
+					//System.out.println(plaintextPassword);					
+				}
+			
+				//Closing Buffer - Done reading dictionary file
+				dictionaryBuffer1.close();
+				
+				//System.out.println("Hello");
+				//Printing out Username::Password matched pairs
+				for (Map.Entry<String, String> entry : usernameToPlaintextPassword.entrySet()) {
+				    System.out.println(entry.getKey()+"::"+entry.getValue());
+				}	
+				//System.out.println("Hello again");
+				
+//				String hp = "cTrpsypRsEoi0Sotz1r0jvkTjTSfA60yxO3RzBRNF3o=";
+//				byte[] hpwd = Base64.getDecoder().decode(hp);
+//				String s = "B9OGLTbJNATU+ZJdnaUUGnMe4hOeK9qRW/6zG+Lkn0E=";
+//				byte[] slt = Base64.getDecoder().decode(s);
+//				char [] p = "computer".toCharArray();
+//				
+//				System.out.println(Base64.getEncoder().encodeToString(hashPassword(p, slt,1, 256)));
+//				
+//				if(Base64.getEncoder().encodeToString(hashPassword(p, slt,1, 256)).equals(hp))
+//					System.out.println(true);
+//				else
+//					System.out.println(false);
+//				
+//				if(Arrays.equals(hashPassword(p, slt,1, 256), hpwd))
+//					System.out.println(true);
+//				else
+//					System.out.println(false);
+				
+				
 				
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
